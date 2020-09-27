23a40a0bd1869d3bda33eba0da2a3fea6097f9f0
angogi <andresmahanaim@gmail.com>
2020-09-26 15:42:02 -1100

    Updates

---
+++ precache-manifest.185ea1269027eb5950869bfbdbf540a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64203c8d79ae9181cfd9c97476e57dea",
+    "url": "/sccisorsnrazors/index.html"
+  },
+  {
+    "revision": "4fb373885c632ccfc919",
+    "url": "/sccisorsnrazors/static/css/main.4dd02ade.chunk.css"
+  },
+  {
+    "revision": "7c3f9742ca7beda106e6",
+    "url": "/sccisorsnrazors/static/js/2.2859ede2.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/sccisorsnrazors/static/js/2.2859ede2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb373885c632ccfc919",
+    "url": "/sccisorsnrazors/static/js/main.36390da4.chunk.js"
+  },
+  {
+    "revision": "c18ffdb690db7df1cce3",
+    "url": "/sccisorsnrazors/static/js/runtime-main.134cd1bf.js"
+  },
+  {
+    "revision": "ea5bdbbca189b685e65728e75aea1822",
+    "url": "/sccisorsnrazors/static/media/224677-P1BLPU-896.ea5bdbbc.jpg"
+  },
+  {
+    "revision": "815ada6c539865f5ef43054a3369c7ae",
+    "url": "/sccisorsnrazors/static/media/Balayage-caramelo-2.815ada6c.jpg"
+  },
+  {
+    "revision": "12e7196f8cdb3ddfdcf82ea2de710a08",
+    "url": "/sccisorsnrazors/static/media/balayage-3.12e7196f.jpg"
+  },
+  {
+    "revision": "43afd13004cebccfc6807c9c1d001525",
+    "url": "/sccisorsnrazors/static/media/masaje.43afd130.jpg"
+  },
+  {
+    "revision": "1a305d0ed55165bf2872eccedd657706",
+    "url": "/sccisorsnrazors/static/media/masaje_facial.1a305d0e.jpg"
+  }
+]);
